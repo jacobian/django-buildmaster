48ff210615a1c0445ca055b1227ad907e45d0cae
Jacob Kaplan-Moss <jacob@jacobian.org>
2010-11-04 11:43:17 -0500

    In retrospect, allowing anonymous access to control the buildmaster was a bad idea.
    Disabling it until I can figure out how auth works.

--- djangobotcfg/status.py
+++ djangobotcfg/status.py
@@ -2,22 +2,22 @@ from buildbot.status import html, words
 from buildbot.status.web.authz import Authz
 from buildbot.status.web.auth import BasicAuth
 
-authz = Authz(
-    forceBuild=True,
-    forceAllBuilds=True,
-    pingBuilder=True,
-    gracefulShutdown=True,
-    stopBuild=True,
-    stopAllBuilds=True,
-    cancelPendingBuild=True,
-    cleanShutdown=True,
-)
+# authz = Authz(
+#     forceBuild=True,
+#     forceAllBuilds=True,
+#     pingBuilder=True,
+#     gracefulShutdown=True,
+#     stopBuild=True,
+#     stopAllBuilds=True,
+#     cancelPendingBuild=True,
+#     cleanShutdown=True,
+# )
 
 def get_status():
     return [
         html.WebStatus(
             http_port = '8010',
-            authz = authz,
+            # authz = authz,
             order_console_by_time = True,
             revlink = 'http://code.djangoproject.com/changeset/%s',
             changecommentlink = (

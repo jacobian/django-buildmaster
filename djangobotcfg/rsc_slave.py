2fe22f8065a21fb7c5fbb930fe8d2334c5f34983
Jacob Kaplan-Moss <jacob@jacobian.org>
2010-11-05 16:53:11 -0500

    Added a bunch of additions to the RSC latent slave to try to get subsequent builds working.
    Unfortunately I failed: something' still not right.

--- djangobotcfg/rsc_slave.py
+++ djangobotcfg/rsc_slave.py
@@ -21,6 +21,7 @@ class CloudserversLatentBuildslave(AbstractLatentBuildSlave):
         self.image = self.get_image(image)
         self.flavor = self.get_flavor(flavor)
         self.instance = None
+        self.instance_lock = defer.DeferredLock()
         
         # Shut the server down once the build(s) are complete?
         self.insubstantiate_after_build = insubstantiate_after_build
@@ -44,8 +45,8 @@ class CloudserversLatentBuildslave(AbstractLatentBuildSlave):
             return self.conn.flavors.find(name=flavor)
             
     def start_instance(self):
-        if self.instance is not None:
-            raise ValueError('instance active')
+        # Prevent starting a new instance while an old one is shutting down.
+        self.instance_lock.acquire()
         return threads.deferToThread(self._start_instance)
     
     def _start_instance(self):
@@ -55,22 +56,43 @@ class CloudserversLatentBuildslave(AbstractLatentBuildSlave):
         
         duration = 0
         while self.instance.status == 'BUILD':
+            self.instance.get()
             time.sleep(5)
             duration += 5
             if duration % 60 == 0:
                 log.msg('%s %s has waited %d seconds for instance %s' %
                         (self.__class__.__name__, self.slavename, duration, self.instance.id))
+        
+        # Sometimes status goes BUILD -> UNKNOWN briefly before coming ACTIVE.
+        # So we'll wait for it in the UNKNOWN state for a bit.
+        duration = 0
+        while self.instance.status == 'UNKNOWN':
             self.instance.get()
-        
-        # FIXME: sometimes status goes BUILD -> UNKNOWN -> ACTIVE briefly.
-        #        just wait for it to become ACTIVE? Or set some time out?
-        #        Also, can we re-try? Sometimes booting doesn't work right.
-        
+            time.sleep(5)
+            duration += 5
+            if duration % 60 == 0:
+                log.msg('%s %s instance %s has been UNKNOWN for %d seconds' % 
+                        (self.__class__.__name__, self.slavename, duration, self.instance.id))
+            if duration == 600:
+                log.msg('%s %s giving up on instance %s after UNKNOWN for 10 minutes.' % 
+                        (self.__class__.__name__, self.slavename, self.instance.id))
+                raise LatentBuildSlaveFailedToSubstantiate(self.instance.id, self.instance.status)
+            
+        # XXX Sometimes booting just... fails. When that happens the slave
+        # basically becomes "stuck" and Buildbot won't do anything more with it.
+        # So should we re-try here? Or set self.instance to None? Or...?
         if self.instance.status != 'ACTIVE':
-            log.msg('%s %s failed to start instance %s (status=%s)' %
+            log.msg('%s %s failed to start instance %s (status became %s)' %
                     (self.__class__.__name__, self.slavename, self.instance.id, self.instance.status))
             raise LatentBuildSlaveFailedToSubstantiate(self.instance.id, self.instance.status)
-            
+        
+        # Also, sometimes the slave boots but just doesn't actually come up
+        # (it's alive but networking is broken?) A hard reboot fixes it most
+        # of the time, so ideally we'd expect to hear from the slave within
+        # some minutes and issue a hard reboot (or kill it and try again?)
+        # Is that possible from here?
+        
+        # FIXME: this message prints the wrong number of seconds.
         log.msg('%s %s instance %s started in about %d seconds' %
                 (self.__class__.__name__, self.slavename, self.instance.id, duration))
         return self.instance.id
@@ -78,12 +100,46 @@ class CloudserversLatentBuildslave(AbstractLatentBuildSlave):
     def stop_instance(self, fast=False):
         if self.instance is None:
             return defer.succeed(None)
-        return threads.deferToThread(self._stop_instance, self.instance)
+            
+        instance, self.instance = self.instance, None
+        d = threads.deferToThread(self._stop_instance, instance)
+    
+        # Release the lock when _stop_instance succeeds.
+        @d.addCallback
+        def _done(res):
+            return self.instance_lock.release()
+            
+        # XXX Is this needed? Coppied from http://buildbot.net/trac/ticket/1001
+        @d.addCallback
+        def _released(res):
+            return True
+            
+        return d
+        
+        # XXX It seems like this might work, too:
+        # return threads.deferToThread(self._stop_instance, instance).addCallback(self.instance_lock.release)
     
     def _stop_instance(self, instance):
         instance.delete()
-        # FIXME: this needs to wait until the slave goes away.
-        #        ... and set self.instance = None?
+        
+        # Wait for the instance to go away. We can't just wait for a deleted
+        # state, unfortunately -- the resource just goes away and we get a 404.
+        try:
+            duration = 0
+            while instance.status == 'ACTIVE':
+                instance.get()
+                time.sleep(5)
+                duration += 5
+                if duration % 60 == 0:
+                    log.msg('%s %s has waited %s for instance %s to die' % 
+                            (self.__class__.__name__, self.slavename, duration, self.instance.id))
+                    # Try to delete it again, just for funsies.
+                    instance.delete()
+        except cloudservers.NotFound:
+            # We expect this NotFound - it's what happens when the slave dies.
+            pass
+            
+        
         log.msg('%s %s deleted instance %s' % 
                 (self.__class__.__name__, self.slavename, instance.id))
                 

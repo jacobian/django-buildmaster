b9e4d88d4b39b57260fcae49b3c1a52fe265b16d
Jacob Kaplan-Moss <jacob@jacobian.org>
2010-11-05 16:21:37 -0500

    You got Django in my Buildbot! You got Buildbot in my Django!

--- djangobotcfg/status.py
+++ djangobotcfg/status.py
@@ -1,12 +1,25 @@
 from buildbot.status import html, words
 from buildbot.status.web.authz import Authz
-from buildbot.status.web.auth import BasicAuth
+from .djangoauth import DjangoAuth
+
+authz = Authz(
+    auth = DjangoAuth(),
+    gracefulShutdown = 'auth',
+    forceBuild = 'auth',
+    forceAllBuilds = 'auth',
+    pingBuilder = 'auth',
+    stopBuild = 'auth',
+    stopAllBuilds = 'auth',
+    cancelPendingBuild = 'auth',
+    stopChange = 'auth',
+    cleanShutdown = 'auth',
+)
 
 def get_status():
     return [
         html.WebStatus(
             http_port = '8010',
-            # authz = authz,
+            authz = authz,
             order_console_by_time = True,
             revlink = 'http://code.djangoproject.com/changeset/%s',
             changecommentlink = (
